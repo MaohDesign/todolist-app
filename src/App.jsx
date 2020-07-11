f985ffc40fc007917fe723285cf97d7d4618629b
Mami Ohira <maoh.design@gmail.com>
2020-07-11 21:14:58 +0900

    Add react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ReactQueryDevtools initialIsOpen />
     </div>
   );
 }
